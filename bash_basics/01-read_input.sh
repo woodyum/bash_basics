fd7125aad5c74626be2f3751ab7f09e0773600fe
woodyum <vum@ggc.edu>
2024-09-20 12:14:44 -0400

    updated new words to the bash script

--- bash_basics/01-read_input.sh
+++ bash_basics/01-read_input.sh
@@ -3,7 +3,7 @@
 
 echo "Hello! What is your name"
 read name
-echo "Welcome, $name"
+echo "Your name is $name"
 
 # single quotes prevent the expansion of the variable
 echo 'Your name was stored in $name'
